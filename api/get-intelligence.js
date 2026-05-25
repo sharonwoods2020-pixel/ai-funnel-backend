0304a77e16a6e21f835ec78705f73f87f4ae5853
sharonwoods2020-pixel <sharonwoods2020@gmail.com>
2026-05-24 20:32:05 -0700

    Update get-intelligence.js

--- api/get-intelligence.js
+++ api/get-intelligence.js
@@ -124,7 +124,7 @@ export default async function handler(req, res) {
         backgroundImage: heroImage,
         heroImage,
       },
-
+ 
       template: {
         coverImage: heroImage,
       },
