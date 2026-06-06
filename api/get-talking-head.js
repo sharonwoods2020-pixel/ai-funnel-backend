c168b89576ccc425c5459889273c801e68f577c2
sharonwoods2020-pixel <sharonwoods2020@gmail.com>
2026-06-06 16:43:31 -0400

    Update get-talking-head.js

--- api/get-talking-head.js
+++ api/get-talking-head.js
@@ -1,1 +1,15 @@
+export default async function handler(req, res) {
+  try {
+    return res.status(200).json({
+      success: true,
+      message: "Talking Head API Ready"
+    });
+  } catch (error) {
+    console.error("get-talking-head error:", error);
 
+    return res.status(500).json({
+      success: false,
+      error: error.message
+    });
+  }
+}
